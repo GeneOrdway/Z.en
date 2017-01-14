9247ca165c0469db73576210255a5f1fdd5b42bb
Gene Ordway <GeneOrdway@GMail.com>
2017-01-13 21:23:30 -0500

    Updated bytes2human.sh

--- current/libexec/bytes2human.sh
+++ current/libexec/bytes2human.sh
@@ -42,7 +42,9 @@ ARRAY_FILE_SIZES_NAMES=(B KB MB GB TB EB PB YB ZB)
 ### FUNCTIONS ###
 ###           ###
 
-
+fn_HELP() {
+    $PRINTF "This is the help menu.\n"
+}
 
 ###      ###
 ### MAIN ###
@@ -53,37 +55,45 @@ ARRAY_FILE_SIZES[$CURRENT_STEP]=$1
 
 # Debug:
 if [ $DEBUG_FLAG -eq 0 ]; then
-    $PRINTF "NEXT_STEP is: $NEXT_STEP\n"
     $PRINTF "CURRENT_STEP is: $CURRENT_STEP\n"
+    $PRINTF "NEXT_STEP is: $NEXT_STEP\n"
     $PRINTF "ARRAY_FILE_SIZES[$CURRENT_STEP] is: ${ARRAY_FILE_SIZES[$CURRENT_STEP]}\n"
+    $PRINTF "X-----\n"
 fi
 
-#
-#while [ ${ARRAY_FILE_SIZES[$CURRENT_STEP]} -ge 1024 ]
-##while [ $CURRENT_STEP -gt 0 ]
-##do 
+# I couldn't figure out the logic to get this to work. Tried another way.
+##while [ ${ARRAY_FILE_SIZES[$CURRENT_STEP]} -ge 1024 ]
+while [ $CURRENT_STEP -ne $NEXT_STEP ]
+do 
 
     if [ ${ARRAY_FILE_SIZES[$CURRENT_STEP]} -ge 1024 ]; then
-    # We need to use bc because we require floating point numbers.
-    ARRAY_FILE_SIZES[$NEXT_STEP]=`$PRINTF "scale=2; ${ARRAY_FILE_SIZES[$CURRENT_STEP]} / 1024 \n" | $BC`
+        # We need to use bc because we require floating point numbers.
+        ARRAY_FILE_SIZES[$NEXT_STEP]=`$PRINTF "scale=2; ${ARRAY_FILE_SIZES[$CURRENT_STEP]} / 1024 \n" | $BC`
 
-    if [ $DEBUG_FLAG -eq 0 ]; then
-        $PRINTF "ARRAY_FILE_SIZES[$CURRENT_STEP] is: ${ARRAY_FILE_SIZES[$CURRENT_STEP]} \n"
-        $PRINTF "ARRAY_FILE_SIZES[$NEXT_STEP] is: ${ARRAY_FILE_SIZES[$NEXT_STEP]} \n"
-    fi
-
-    # Store the converted number into a new element of the array.
-    #ARRAY_FILE_SIZES[$CURRENT_STEP]=$CONVERSION
-
-    # Print  
+        if [ $DEBUG_FLAG -eq 0 ]; then
+            $PRINTF "ARRAY_FILE_SIZES[$CURRENT_STEP] is: ${ARRAY_FILE_SIZES[$CURRENT_STEP]} \n"
+            $PRINTF "ARRAY_FILE_SIZES[$NEXT_STEP] is: ${ARRAY_FILE_SIZES[$NEXT_STEP]} \n"
+            $PRINTF "X-----\n"
+        fi 
     
-    # Increment
+        # Increment NEXT_STEP
+        ((NEXT_STEP++))
+
+        if [ $DEBUG_FLAG -eq 0 ]; then
+            $PRINTF "CURRENT_STEP is: $CURRENT_STEP\n"
+            $PRINTF "NEXT_STEP is: $NEXT_STEP\n"
+            $PRINTF "X-----\n"
+        fi
+
+    fi
+    # Increment CURRENT_STEP
     ((CURRENT_STEP++))
 
-    $PRINTF "CURRENT_STEP is: $CURRENT_STEP\n"
-    $PRINTF "NEXT_STEP is: $NEXT_STEP\n"
+done
 
-    fi
-##done
+# Output final number:
+#$PRINTF "${ARRAY_FILE_SIZES[*]:1}\n"
+$PRINTF "${ARRAY_FILE_SIZES[$CURRENT_STEP]}\n"
 
+exit 0
 #EOF
