a22f40754409fce9b0091cb09a1c7ae232d2ca1f
Gene Ordway <GeneOrdway@GMail.com>
2017-06-27 04:04:08 -0400

    Updated ../libexec/gmailnotifications.sh and added ../etc/gmailnotifications.conf

--- current/libexec/gmailnotifications.sh
+++ current/libexec/gmailnotifications.sh
@@ -1,25 +1,73 @@
 #!/bin/sh 
 #
-# Script to check for email notifications
+# Script to check for gmail notifications
 
 ###       ###
 ### TO DO ###
 ###       ###
 
-# 1 - 
+# 1 - So after thinking about this script, this is a quick and dirty hack
+#       to check for new emails on Google's gmail platform, but there
+#       are better ways of achieving the same results; namely, by
+#       connecting to email servers via IMAP instances and allowing
+#       a new connection to poll for emails. This will also allow for
+#       connections to a multitude of email platforms, not just gmail.
+# 2 - If we do intend to keep this script, we cannot check more frequently
+#       than once every 10 minutes.
+# 3 - Finish adding error checking to this script.
+# 4 - 
 
 ###          ###
 ### PROGRAMS ###
 ###          ###
+CURL="/usr/bin/curl"
+XMLLINT="/usr/bin/xmllint"
+PRINTF="/usr/bin/printf"
 
+###           ###
+### VARIABLES ###
+###           ###
+
+CONFIGURATION_FILE="$HOME/.etc/gmailnotifications.conf"
+
+# Initialize:
+USERNAME=""
+PASSWORD=""
+
+###                ###
+### ERROR CHECKING ###
+###                ###
+
+# Does PRINTF exist?
+
+# Does CURL exist?
+
+# Does XMLLINT exist?
+
+# Read in source:
+if [[ ! -e "$CONFIGURATION_FILE" ]]; then
+    exit 1
+else
+    source $CONFIGURATION_FILE 
+fi
+
+# Blank username or password? Get outta' here!
+if [[ ! -e "$USERNAME" || ! -e "$PASSWORD" ]]; then
+    exit 1
+fi
+
+# Can we support emojis?
 
 ###      ###
 ### MAIN ###
 ###      ###
+EMAIL_CHECK=`$CURL -u $USERNAME:$PASSWORD --silent "https://mail.google.com/mail/feed/atom" | $XMLLINT --xpath "string(//*[name() = 'feed']/*[name() = 'fullcount'])" - `
 
-username="USERNAME"
-password="PASSWORD"
-echo
-curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" |  grep -oPm1 "(?<=<title>)[^<]+" | sed '1d'
+if [ "$EMAIL_CHECK" -ge 1 ]; then 
+    $PRINTF "📬  $EMAIL_CHECK\n"
+else
+    $PRINTF "📭 \n"
+fi
 
+exit 0
 #EOF
